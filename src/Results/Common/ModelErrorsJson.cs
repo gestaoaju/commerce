e5d0a83df9becaac806f4b4ad7891b215273bd3a
Marx JMoura <marx.jmoura@gmail.com>
2017-08-06 17:34:51 -0300

    Implement common results

---
+++ src/Results/Common/ModelErrorsJson.cs
@@ -0,0 +1,16 @@
+// Copyright (c) gestaoaju.com.br - All rights reserved.
+// Licensed under MIT (https://github.com/gestaoaju/commerce/blob/master/LICENSE).
+
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Gestaoaju.Results.Common
+{
+    public class ModelErrorsJson : JsonResult
+    {
+        public ModelErrorsJson(params string[] errors) : base(new { errors = errors })
+        {
+            StatusCode = StatusCodes.Status422UnprocessableEntity;
+        }
+    }
+}
